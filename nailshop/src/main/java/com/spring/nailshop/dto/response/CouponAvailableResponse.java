e00bb0d5e1d5819b641499bb123da87073a736eb
Cuongtran6969 <cuongtvhe176258@fpt.edu.vn>
2025-01-01 17:02:24 +0700

    voucher for cart

--- nailshop/src/main/java/com/spring/nailshop/dto/response/CouponAvailableResponse.java
+++ nailshop/src/main/java/com/spring/nailshop/dto/response/CouponAvailableResponse.java
@@ -10,7 +10,7 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CouponResponse {
-    CouponType type;
+    String type;
     Double amount;
     Boolean isAvailable;
 }
